dad295d25ea469916024fb3273664936c0e1a4d9
Larry <yunpengliu199703@gmail.com>
2017-04-13 15:55:00 -0700

    initialize the Game

---
+++ Trajectory/Assets/C# script/Drawing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawing : MonoBehaviour {
+
+
+	public float speed = 5.0f; //initial speed of the trajectory
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate()
+	{
+	
+	
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
